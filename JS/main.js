6f275efb6350e787df0d86628cfc484912aeb8dc
EVA <edie.blues@gmail.com>
2018-02-26 07:40:03 +0000

    random cat image generator mac terminal style

---
+++ JS/main.js
@@ -0,0 +1,21 @@
+
+document.addEventListener('keydown', function (event) {
+  if (event.which === 13) {
+  	location.reload(true); 	
+  }
+});
+
+// var img = document.getElementById('image');
+// document.addEventListener("keydown", function(e){
+// 	if (e.which===13){
+// 		newSrc();
+
+// 	}
+// })
+
+
+
+// function newSrc() {
+//   img.setAttribute('src', "https://source.unsplash.com/collection/139386/250x250");
+//   console.log(img)
+// }
